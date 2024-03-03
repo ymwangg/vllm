15b3de4fd6aac9844823878f2f4f24ddba215cfa
Yanming Wang <yanmwang@amazon.com>
2024-03-03 01:04:47 +0000

    Use fast greedy sampling for draft model

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -982,6 +982,169 @@ class ModelRunner:
         # This usually takes < 10 seconds.
         logger.info(f"Graph capturing finished in {elapsed_time:.0f} secs.")
 
+    def prepare_draft_input_tensors(
+        self,
+        seq_group_metadata_list: List[SequenceGroupMetadata],
+    ) -> Tuple[torch.Tensor, torch.Tensor, InputMetadata]:
+        assert len(seq_group_metadata_list) > 0
+        input_tokens: List[List[int]] = []
+        input_positions: List[List[int]] = []
+        slot_mapping: List[List[int]] = []
+        context_lens: List[int] = []
+        block_tables: List[List[int]] = []
+        for seq_group_metadata in seq_group_metadata_list:
+            assert not seq_group_metadata.is_prompt
+
+            seq_ids = list(seq_group_metadata.seq_data.keys())
+            assert len(seq_ids) == 1
+            seq_id = seq_ids[0]
+
+            seq_data = seq_group_metadata.seq_data[seq_id]
+
+            token_id = seq_data.get_last_draft_token_id() if \
+                seq_data.get_num_draft_tokens() > 0 else seq_data.get_last_token_id()
+            generation_tokens = [token_id]
+
+            input_tokens.append(generation_tokens)
+            seq_len = seq_data.get_len() + seq_data.get_num_draft_tokens()
+
+            context_len = seq_len if self.sliding_window is None else min(
+                seq_len, self.sliding_window)
+            context_lens.append(context_len)
+
+            input_positions.append([seq_len - 1])
+
+            block_table = seq_group_metadata.d_block_tables[seq_id]
+
+            position = seq_len - 1
+            block_number = block_table[position // self.block_size]
+            block_offset = position % self.block_size
+            slot = block_number * self.block_size + block_offset
+            slot_mapping.append([slot])
+            if self.sliding_window is not None:
+                sliding_window_blocks = (self.sliding_window //
+                                         self.block_size)
+                block_table = block_table[-sliding_window_blocks:]
+            block_tables.append(block_table)
+
+        batch_size = len(input_tokens)
+        # record the real batch size for cudagraph in case of padding
+        real_batch_size = batch_size
+        max_context_len = max(context_lens)
+        use_captured_graph = (
+            not self.model_config.enforce_eager
+            and batch_size <= _BATCH_SIZES_TO_CAPTURE[-1]
+            and max_context_len <= self.max_context_len_to_capture)
+        if use_captured_graph:
+            # Pad the input tokens, positions, and slot mapping to match the
+            # batch size of the captured graph.
+            graph_batch_size = _get_graph_batch_size(batch_size)
+            assert graph_batch_size >= batch_size
+            for _ in range(graph_batch_size - batch_size):
+                input_tokens.append([])
+                input_positions.append([])
+                slot_mapping.append([])
+                context_lens.append(1)
+                block_tables.append([])
+            batch_size = graph_batch_size
+
+        max_len = max([len(t) for t in input_tokens])
+        input_tokens = _make_tensor_with_pad(input_tokens,
+                                             max_len=max_len,
+                                             pad=0,
+                                             dtype=torch.long,
+                                             device=self.device)
+        input_positions = _make_tensor_with_pad(input_positions,
+                                                max_len=max_len,
+                                                pad=0,
+                                                dtype=torch.long,
+                                                device=self.device)
+        slot_mapping = _make_tensor_with_pad(slot_mapping,
+                                             max_len=max_len,
+                                             pad=_PAD_SLOT_ID,
+                                             dtype=torch.long,
+                                             device=self.device)
+        context_lens = torch.tensor(context_lens,
+                                    dtype=torch.int,
+                                    device=self.device)
+
+        if use_captured_graph:
+            # The shape of graph_block_tables is
+            # [max batch size, max context len // block size].
+            input_block_tables = self.graph_block_tables[:batch_size]
+            for i, block_table in enumerate(block_tables):
+                if block_table:
+                    input_block_tables[i, :len(block_table)] = block_table
+            block_tables = torch.tensor(input_block_tables, device=self.device)
+        else:
+            max_block_table_len = max(
+                len(block_table) for block_table in block_tables)
+            block_tables = _make_tensor_with_pad(
+                block_tables,
+                max_len=max_block_table_len,
+                pad=0,
+                dtype=torch.int,
+                device=self.device,
+            )
+
+        input_metadata = InputMetadata(
+            is_prompt=False,
+            slot_mapping=slot_mapping,
+            prompt_lens=None,
+            max_seq_len=None,
+            start_loc=None,
+            max_context_len=max_context_len,
+            context_lens=context_lens,
+            block_tables=block_tables,
+            use_cuda_graph=use_captured_graph,
+            kv_cache_dtype=self.kv_cache_dtype,
+            use_speculate=self.use_speculate,
+            is_multi_query_mode=False,
+            batch_size=real_batch_size,
+        )
+        return (input_tokens, input_positions, input_metadata)
+
+    def draft_step(
+        self,
+        seq_group_metadata_list: List[SequenceGroupMetadata],
+        kv_caches: List[Tuple[torch.Tensor, torch.Tensor]],
+    ):
+        if not self.is_driver_worker:
+            return None
+        input_tokens, input_positions, input_metadata = self.prepare_draft_input_tensors(
+            seq_group_metadata_list)
+        # special faster draft model step
+        tp_size = self.parallel_config.draft_model_tp_size
+        if self.rank >= tp_size:
+            return
+        with MarkActiveModel(ActiveModel.DRAFT):
+            graph_batch_size = input_tokens.shape[0]
+            model = self.draft_model
+            model_executable = self.draft_model
+            if input_metadata.use_cuda_graph:
+                model_executable = self.d_graph_runners[graph_batch_size]
+            # Execute the model.
+            hidden_states = model_executable(
+                input_ids=input_tokens,
+                positions=input_positions,
+                kv_caches=kv_caches,
+                input_metadata=input_metadata,
+            )
+            logits = torch.matmul(hidden_states.squeeze(1),
+                                  model.lm_head.weight.t())
+            probs = torch.softmax(logits, dim=-1)
+            output = torch.argmax(probs, dim=-1)
+            token_ids_list = output.tolist()
+            probs_list = torch.split(probs, 1, dim=0)
+
+        for seq_group_metadata, token_id, parent_probs in zip(
+                seq_group_metadata_list, token_ids_list, probs_list):
+            seq_ids = list(seq_group_metadata.seq_data.keys())
+            assert len(seq_ids) == 1
+            seq_id = seq_ids[0]
+            seq_data = seq_group_metadata.seq_data[seq_id]
+            seq_data.append_draft_token(token_id, None, parent_probs)
+
     def speculate_step(
         self,
         seq_group_metadata_list: List[SequenceGroupMetadata],
@@ -1130,9 +1293,10 @@ class ModelRunner:
         # Token generation
         # Step 1: Generate draft tokens
         for _ in range(self.speculate_length):
-            self.speculate_step(seq_group_metadata_list,
-                                d_kv_caches,
-                                is_draft_model=True)
+            # self.speculate_step(seq_group_metadata_list,
+            #                     d_kv_caches,
+            #                     is_draft_model=True)
+            self.draft_step(seq_group_metadata_list, d_kv_caches)
         # Step 2: Run target model to evaluate draft tokens
         output = self.speculate_step(seq_group_metadata_list,
                                      kv_caches,
